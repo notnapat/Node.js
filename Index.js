169828c5d44b059267e29ee7b3b3fb86058df84f
notnapat <notnapat9@gmail.com>
2022-06-11 21:44:56 +0700

    Modules fs  Synchronous(Blocking) อ่าน และ เขียนไฟล์  Index.txt + input.js + output.txt

--- Index.js
+++ Index.js
@@ -1,16 +1,18 @@
-// Modules fs  อ่าน และ เขียนไฟล์  Index.txt + input.js + output.txt
+// Modules fs  Synchronous(Blocking) อ่าน และ เขียนไฟล์  Index.txt + input.js + output.txt
 const fs = require('fs')
 
-//Modu fs อ่านไฟล์จาก input.txt
+//Modu fs Syn อ่านไฟล์จาก input.txt
+
 const data = fs.readFileSync('myFile/input.txt','utf-8')
 console.log(data)
 
-//Modu fa เขียนไฟล์ เพิ่มข้อมูลไปยัง output.txt
+//Modu fs Syn เขียนไฟล์ เพิ่มข้อมูลไปยัง output.txt
 const outputText = `สร้างไฟล์ output.txt พร้อมกับเขียนข้อมูลลงไป\n${data}\nไฟล์ถูกเขียนเมื่อ${new Date()}`
 fs.writeFileSync("myFile/output.txt",outputText)
-console.log(เขียนไฟล์เสร็จเรียบร้อย)
+console.log(outputText)
+console.log("เขียนไฟล์เสร็จเรียบร้อย")
 
-_______________________________________________________________________
+//_______________________________________________________________________
 
 // // Modules Basic  Index.js + mymodules.js = ดึงฟังชั่นจากอีกไฟล์มาใช้
 // const mymodules = require ('./modules/mymodules')
