c86b57802d84f56a0d7f8312e230741b609f4a07
notnapat <notnapat9@gmail.com>
2022-06-11 17:50:12 +0700

    Modules fs  อ่าน และ เขียนไฟล์  Index.txt + input.js + output.txt

--- Index.js
+++ Index.js
@@ -1,8 +1,22 @@
-// Modules Basic  Index.js + mymodules.js = ดึงฟังชั่นจากอีกไฟล์มาใช้
-const mymodules = require ('./modules/mymodules')
+// Modules fs  อ่าน และ เขียนไฟล์  Index.txt + input.js + output.txt
+const fs = require('fs')
 
-const number = 300000
+//Modu fs อ่านไฟล์จาก input.txt
+const data = fs.readFileSync('myFile/input.txt','utf-8')
+console.log(data)
 
-console.log(mymodules.getCurrenttime())
-console.log(mymodules.add(40,20))
-console.log(mymodules.formatNumber(number))
+//Modu fa เขียนไฟล์ เพิ่มข้อมูลไปยัง output.txt
+const outputText = `สร้างไฟล์ output.txt พร้อมกับเขียนข้อมูลลงไป\n${data}\nไฟล์ถูกเขียนเมื่อ${new Date()}`
+fs.writeFileSync("myFile/output.txt",outputText)
+console.log(เขียนไฟล์เสร็จเรียบร้อย)
+
+_______________________________________________________________________
+
+// // Modules Basic  Index.js + mymodules.js = ดึงฟังชั่นจากอีกไฟล์มาใช้
+// const mymodules = require ('./modules/mymodules')
+
+// const number = 300000
+
+// console.log(mymodules.getCurrenttime())
+// console.log(mymodules.add(40,20))
+// console.log(mymodules.formatNumber(number))
